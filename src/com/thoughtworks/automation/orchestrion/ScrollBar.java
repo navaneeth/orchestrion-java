989b0452fbd8ad80ab47eaaa48e934468ef18650
Navaneeth.K.N <navaneethkn@gmail.com>
2013-05-27 12:24:11 +0530

    getValue() on ScrollBar

--- src/com/thoughtworks/automation/orchestrion/ScrollBar.java
+++ src/com/thoughtworks/automation/orchestrion/ScrollBar.java
@@ -8,20 +8,48 @@ public abstract class ScrollBar {
 		this.refId = refId;
 	}
 	
+	/**
+	 * Gets the current value
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	public int getValue() throws Exception {
+		return RemoteServer.instance().executeAndGetId("getvalue", refId);
+	}
+	
+	/**
+	 * Gets the maximum value
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	public int getMaxValue() throws Exception {
 		return RemoteServer.instance().executeAndGetId("getmaxvalue", refId);
 	}
 	
+	/**
+	 * Gets the minimum value
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	public int getMinValue() throws Exception {
 		return RemoteServer.instance().executeAndGetId("getminvalue", refId);
 	}
 	
+	/**
+	 * Gets a value indicating whether this scroll bar can be scrolled
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	public boolean canScroll() throws Exception {
 		return Boolean.parseBoolean(RemoteServer.instance().execute("canscroll", refId));
-	}
+	}	
 	
 	protected int getRefId() {
 		return refId;
-	}
+	}	
 
 }
