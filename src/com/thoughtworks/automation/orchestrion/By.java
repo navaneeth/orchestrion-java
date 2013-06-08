bc9f8c4c49c3f4dd3e93a4cc527560f1ff69e7c2
Navaneeth.K.N <navaneethkn@gmail.com>
2013-06-08 21:52:22 +0530

    Adding more search criteria

--- src/com/thoughtworks/automation/orchestrion/By.java
+++ src/com/thoughtworks/automation/orchestrion/By.java
@@ -3,69 +3,123 @@ package com.thoughtworks.automation.orchestrion;
 /**
  * 
  * Represents search criteria for finding UI items
- *
+ * 
  */
 public abstract class By {
-	
+
 	/**
 	 * Locate UI item based on automation id
 	 * 
-	 * @param automationId An ID for an element that is unique among siblings within its container.
+	 * @param automationId
+	 *            An ID for an element that is unique among siblings within its
+	 *            container.
 	 * @return
 	 */
 	public static By automationId(String automationId) {
 		return new ByAutomationId(automationId);
 	}
-	
+
 	/**
-	 * Locate UI items based on text. 
+	 * Locate UI item based on text.
 	 * <p>
-	 * For eg: a button with text 'OK' can be located by 
+	 * For eg: a button with text 'OK' can be located by
+	 * 
 	 * <pre>
 	 * {@code window.getButton(By.text("OK"))}
 	 * </pre>
+	 * 
 	 * @param text
 	 * @return
 	 */
 	public static By text(String text) {
 		return new ByText(text);
 	}
-	
+
+	/**
+	 * Locate UI item based on class name
+	 * <p>
+	 * Class name can be located from UISpy or inspect tool
+	 * 
+	 * @param className
+	 * @return
+	 */
+	public static By className(String className) {
+		return new ByClassName(className);
+	}
+
+	/**
+	 * Locate UI item based on framework
+	 * 
+	 * @param framework
+	 * @return
+	 */
+	public static By framework(String framework) {
+		return new ByFramework(framework);
+	}
+
 	private final String value;
+
 	protected By(String value) {
 		this.value = value;
-		
+
 	}
-	
+
 	public abstract String getKey();
+
 	public String getValue() {
 		return value;
 	}
-	
+
 	private static class ByAutomationId extends By {
 
 		public ByAutomationId(String automationId) {
 			super(automationId);
 		}
-		
+
 		@Override
 		public String getKey() {
 			return "automationid";
 		}
-		
+
 	}
-	
+
 	private static class ByText extends By {
 
 		public ByText(String text) {
 			super(text);
 		}
-		
+
 		@Override
 		public String getKey() {
 			return "text";
 		}
-		
+
+	}
+
+	private static class ByClassName extends By {
+
+		public ByClassName(String className) {
+			super(className);
+		}
+
+		@Override
+		public String getKey() {
+			return "classname";
+		}
+
+	}
+
+	private static class ByFramework extends By {
+
+		public ByFramework(String framework) {
+			super(framework);
+		}
+
+		@Override
+		public String getKey() {
+			return "framework";
+		}
+
 	}
 
 }
